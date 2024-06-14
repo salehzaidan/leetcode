7efd77bff80bc0d70bfee4e9d5dda5812fcefc3e
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-06-14 21:41:55 +0700

    Solve two-sum with one-pass hash table

--- problems/two-sum/solution.cpp
+++ problems/two-sum/solution.cpp
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution1 {
   public:
     vector<int> twoSum(vector<int> &nums, int target) {
         for (int i = 0; i < nums.size(); i++) {
@@ -19,8 +20,8 @@ class Solution {
     }
 };
 
-TEST(Solution, TwoSum1) {
-    Solution solution;
+TEST(Solution1, TwoSum1) {
+    Solution1 solution;
     vector<int> expected{0, 1};
     vector<int> nums{2, 7, 11, 15};
     int target = 9;
@@ -28,8 +29,8 @@ TEST(Solution, TwoSum1) {
     EXPECT_EQ(expected, actual);
 }
 
-TEST(Solution, TwoSum2) {
-    Solution solution;
+TEST(Solution1, TwoSum2) {
+    Solution1 solution;
     vector<int> expected{1, 2};
     vector<int> nums{3, 2, 4};
     int target = 6;
@@ -37,8 +38,62 @@ TEST(Solution, TwoSum2) {
     EXPECT_EQ(expected, actual);
 }
 
-TEST(Solution, TwoSum3) {
-    Solution solution;
+TEST(Solution1, TwoSum3) {
+    Solution1 solution;
+    vector<int> expected{0, 1};
+    vector<int> nums{3, 3};
+    int target = 6;
+    vector<int> actual = solution.twoSum(nums, target);
+    EXPECT_EQ(expected, actual);
+}
+
+class Solution2 {
+  public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> indices;
+
+        for (int i = 0; i < nums.size(); i++) {
+            auto a = nums.at(i);
+            indices.insert({a, i});
+
+            auto b = target - a;
+            if (!indices.contains(b)) {
+                continue;
+            }
+
+            auto j = indices.at(b);
+            if (i < j) {
+                return {i, j};
+            }
+            if (i > j) {
+                return {j, i};
+            }
+        }
+
+        throw std::runtime_error("Unreachable");
+    }
+};
+
+TEST(Solution2, TwoSum1) {
+    Solution2 solution;
+    vector<int> expected{0, 1};
+    vector<int> nums{2, 7, 11, 15};
+    int target = 9;
+    vector<int> actual = solution.twoSum(nums, target);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(Solution2, TwoSum2) {
+    Solution2 solution;
+    vector<int> expected{1, 2};
+    vector<int> nums{3, 2, 4};
+    int target = 6;
+    vector<int> actual = solution.twoSum(nums, target);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(Solution2, TwoSum3) {
+    Solution2 solution;
     vector<int> expected{0, 1};
     vector<int> nums{3, 3};
     int target = 6;
