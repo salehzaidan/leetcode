264ba0beea9946fc25fec25b0833ce8d12872384
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-06-23 11:11:53 +0700

    Solve binary-tree-inorder-traversal iteratively

--- problems/binary-tree-inorder-traversal/solution.cpp
+++ problems/binary-tree-inorder-traversal/solution.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <stack>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ struct TreeNode {
         : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution1 {
   public:
     vector<int> inorderTraversal(TreeNode *root) {
         vector<int> values;
@@ -32,8 +33,8 @@ class Solution {
     }
 };
 
-TEST(Solution, InorderTraversal1) {
-    Solution solution;
+TEST(Solution1, InorderTraversal1) {
+    Solution1 solution;
     TreeNode *node1 = new TreeNode(3);
     TreeNode *node2 = new TreeNode(2, node1, nullptr);
     TreeNode *node3 = new TreeNode(1, nullptr, node2);
@@ -45,19 +46,106 @@ TEST(Solution, InorderTraversal1) {
     delete node3;
 }
 
-TEST(Solution, InorderTraversal2) {
-    Solution solution;
+TEST(Solution1, InorderTraversal2) {
+    Solution1 solution;
     TreeNode *node = nullptr;
     vector<int> expected{};
     vector<int> actual = solution.inorderTraversal(node);
     EXPECT_EQ(expected, actual);
 }
 
-TEST(Solution, InorderTraversal3) {
-    Solution solution;
+TEST(Solution1, InorderTraversal3) {
+    Solution1 solution;
     TreeNode *node = new TreeNode(1);
     vector<int> expected{1};
     vector<int> actual = solution.inorderTraversal(node);
     EXPECT_EQ(expected, actual);
     delete node;
+}
+
+TEST(Solution1, InorderTraversal4) {
+    Solution1 solution;
+    TreeNode *node1 = new TreeNode(4);
+    TreeNode *node2 = new TreeNode(5);
+    TreeNode *node3 = new TreeNode(2, node1, node2);
+    TreeNode *node4 = new TreeNode(3);
+    TreeNode *node5 = new TreeNode(1, node3, node4);
+    vector<int> expected{4, 2, 5, 1, 3};
+    vector<int> actual = solution.inorderTraversal(node5);
+    EXPECT_EQ(expected, actual);
+    delete node1;
+    delete node2;
+    delete node3;
+    delete node4;
+    delete node5;
+}
+
+class Solution2 {
+  public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> values;
+        stack<TreeNode *> st;
+
+        auto node = root;
+        while (node != nullptr || !st.empty()) {
+            while (node != nullptr) {
+                st.push(node);
+                node = node->left;
+            }
+
+            node = st.top();
+            st.pop();
+            values.push_back(node->val);
+            node = node->right;
+        }
+
+        return values;
+    }
+};
+
+TEST(Solution2, InorderTraversal1) {
+    Solution2 solution;
+    TreeNode *node1 = new TreeNode(3);
+    TreeNode *node2 = new TreeNode(2, node1, nullptr);
+    TreeNode *node3 = new TreeNode(1, nullptr, node2);
+    vector<int> expected{1, 3, 2};
+    vector<int> actual = solution.inorderTraversal(node3);
+    EXPECT_EQ(expected, actual);
+    delete node1;
+    delete node2;
+    delete node3;
+}
+
+TEST(Solution2, InorderTraversal2) {
+    Solution2 solution;
+    TreeNode *node = nullptr;
+    vector<int> expected{};
+    vector<int> actual = solution.inorderTraversal(node);
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(Solution2, InorderTraversal3) {
+    Solution2 solution;
+    TreeNode *node = new TreeNode(1);
+    vector<int> expected{1};
+    vector<int> actual = solution.inorderTraversal(node);
+    EXPECT_EQ(expected, actual);
+    delete node;
+}
+
+TEST(Solution2, InorderTraversal4) {
+    Solution2 solution;
+    TreeNode *node1 = new TreeNode(4);
+    TreeNode *node2 = new TreeNode(5);
+    TreeNode *node3 = new TreeNode(2, node1, node2);
+    TreeNode *node4 = new TreeNode(3);
+    TreeNode *node5 = new TreeNode(1, node3, node4);
+    vector<int> expected{4, 2, 5, 1, 3};
+    vector<int> actual = solution.inorderTraversal(node5);
+    EXPECT_EQ(expected, actual);
+    delete node1;
+    delete node2;
+    delete node3;
+    delete node4;
+    delete node5;
 }
