20d0b8b92d20b4883f453095a6622990d09f97b2
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-03 13:18:49 +0700

    Solve valid-anagram with frequency array table

--- src/valid-anagram.cpp
+++ src/valid-anagram.cpp
@@ -32,7 +32,7 @@ public:
   }
 };
 
-TEST(isAnagram, testCase1) {
+TEST(Solution1, testCase1) {
   string s = "anagram";
   string t = "nagaram";
 
@@ -40,10 +40,51 @@ TEST(isAnagram, testCase1) {
   EXPECT_EQ(solution.isAnagram(s, t), true);
 }
 
-TEST(isAnagram, testCase2) {
+TEST(Solution1, testCase2) {
   string s = "rat";
   string t = "car";
 
   Solution1 solution;
+  EXPECT_EQ(solution.isAnagram(s, t), false);
+}
+
+class Solution2 {
+public:
+  bool isAnagram(const string s, const string t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+
+    const int LETTERS_SIZE = 26;
+    vector<int> freq(LETTERS_SIZE);
+
+    for (int i = 0; i < s.length(); i++) {
+      freq[s[i] - 'a']++;
+      freq[t[i] - 'a']--;
+    }
+
+    for (const int &count : freq) {
+      if (count != 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+TEST(Solution2, testCase1) {
+  string s = "anagram";
+  string t = "nagaram";
+
+  Solution2 solution;
+  EXPECT_EQ(solution.isAnagram(s, t), true);
+}
+
+TEST(Solution2, testCase2) {
+  string s = "rat";
+  string t = "car";
+
+  Solution2 solution;
   EXPECT_EQ(solution.isAnagram(s, t), false);
 }
