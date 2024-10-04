76102b5ce495e5865f69d0f30c83e43224f46871
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-04 10:51:05 +0700

    Modify product-of-array-except-self solution
    Change prefix and suffix so that it excludes the element at current
    index

--- src/product-of-array-except-self.cpp
+++ src/product-of-array-except-self.cpp
@@ -6,29 +6,21 @@ using namespace std;
 class Solution1 {
 public:
   vector<int> productExceptSelf(const vector<int> &nums) {
-    vector<int> prefix(nums);
+    vector<int> prefix(nums.size());
+    prefix[0] = 1;
     for (int i = 1; i < nums.size(); i++) {
-      prefix[i] = nums[i] * prefix[i - 1];
+      prefix[i] = prefix[i - 1] * nums[i - 1];
     }
 
-    vector<int> suffix(nums);
+    vector<int> suffix(nums.size());
+    suffix[nums.size() - 1] = 1;
     for (int i = nums.size() - 2; i >= 0; i--) {
-      suffix[i] = nums[i] * suffix[i + 1];
+      suffix[i] = suffix[i + 1] * nums[i + 1];
     }
 
     vector<int> product(nums.size());
     for (int i = 0; i < nums.size(); i++) {
-      int before = 1;
-      if (i != 0) {
-        before = prefix[i - 1];
-      }
-
-      int after = 1;
-      if (i != nums.size() - 1) {
-        after = suffix[i + 1];
-      }
-
-      product[i] = before * after;
+      product[i] = prefix[i] * suffix[i];
     }
 
     return product;
