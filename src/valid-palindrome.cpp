ae0ae1242cf40c41516ab87d44cb0b461b46fcf8
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-05 03:58:27 +0700

    Solve valid-palindrome with two pointers

--- src/valid-palindrome.cpp
+++ src/valid-palindrome.cpp
@@ -39,3 +39,46 @@ TEST(Solution1, testCase3) {
   Solution1 solution;
   EXPECT_EQ(solution.isPalindrome(s), true);
 }
+
+class Solution2 {
+public:
+  bool isPalindrome(string s) {
+    transform(s.begin(), s.end(), s.begin(), [](char c) { return tolower(c); });
+    s.erase(remove_if(s.begin(), s.end(), [](char c) { return !isalnum(c); }),
+            s.end());
+
+    int left = 0;
+    int right = s.length() - 1;
+    while (left < right) {
+      if (s[left] != s[right]) {
+        return false;
+      }
+
+      left++;
+      right--;
+    }
+
+    return true;
+  }
+};
+
+TEST(Solution2, testCase1) {
+  string s = "A man, a plan, a canal: Panama";
+
+  Solution2 solution;
+  EXPECT_EQ(solution.isPalindrome(s), true);
+}
+
+TEST(Solution2, testCase2) {
+  string s = "raceacar";
+
+  Solution2 solution;
+  EXPECT_EQ(solution.isPalindrome(s), false);
+}
+
+TEST(Solution2, testCase3) {
+  string s = " ";
+
+  Solution2 solution;
+  EXPECT_EQ(solution.isPalindrome(s), true);
+}
