67bcf652e623e3458cf2f7d1db489e03dbb6f42f
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-06 19:56:12 +0700

    Solve valid-parentheses

---
+++ src/valid-parentheses.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution1 {
+public:
+  bool isValid(string s) {
+    stack<char> opens;
+
+    for (const char &c : s) {
+      if (c == '(' || c == '[' || c == '{') {
+        opens.push(c);
+      } else {
+        if (opens.empty()) {
+          return false;
+        }
+
+        char top = opens.top();
+        if (!(top == '(' && c == ')' || top == '[' && c == ']' ||
+              top == '{' && c == '}')) {
+          return false;
+        }
+
+        opens.pop();
+      }
+    }
+
+    return opens.empty();
+  }
+};
+
+TEST(Solution1, testCase1) {
+  string s = "()";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), true);
+}
+
+TEST(Solution1, testCase2) {
+  string s = "()[]{}";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), true);
+}
+
+TEST(Solution1, testCase3) {
+  string s = "(]";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), false);
+}
+
+TEST(Solution1, testCase4) {
+  string s = "([])";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), true);
+}
+
+TEST(Solution1, testCase5) {
+  string s = "{";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), false);
+}
+
+TEST(Solution1, testCase6) {
+  string s = ")";
+
+  Solution1 solution;
+  EXPECT_EQ(solution.isValid(s), false);
+}
