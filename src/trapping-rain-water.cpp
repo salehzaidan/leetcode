fdc977b314cae433a1bd515317686b97d183f2a9
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-06 10:49:28 +0700

    Solve trapping-rain-water with constant memory

--- src/trapping-rain-water.cpp
+++ src/trapping-rain-water.cpp
@@ -51,3 +51,50 @@ TEST(Solution1, testCase3) {
   Solution1 solution;
   EXPECT_EQ(solution.trap(height), 1);
 }
+
+class Solution2 {
+public:
+  int trap(vector<int> &height) {
+    int result = 0;
+
+    int maxLeft = height.front();
+    int maxRight = height.back();
+
+    int left = 0;
+    int right = height.size() - 1;
+    while (left < right) {
+      if (maxLeft <= maxRight) {
+        left++;
+        result += max(0, maxLeft - height[left]);
+        maxLeft = max(maxLeft, height[left]);
+      } else {
+        right--;
+        result += max(0, maxRight - height[right]);
+        maxRight = max(maxRight, height[right]);
+      }
+    }
+
+    return result;
+  }
+};
+
+TEST(Solution2, testCase1) {
+  vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
+  Solution2 solution;
+  EXPECT_EQ(solution.trap(height), 6);
+}
+
+TEST(Solution2, testCase2) {
+  vector<int> height = {4, 2, 0, 3, 2, 5};
+
+  Solution2 solution;
+  EXPECT_EQ(solution.trap(height), 9);
+}
+
+TEST(Solution2, testCase3) {
+  vector<int> height = {4, 2, 3};
+
+  Solution2 solution;
+  EXPECT_EQ(solution.trap(height), 1);
+}
