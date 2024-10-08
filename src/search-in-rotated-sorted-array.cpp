2a13ceca8ec9b16106e6dadda5f10479d6e3518b
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-08 15:02:05 +0700

    Solve search-in-rotated-sorted-array

---
+++ src/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,97 @@
+#include <gtest/gtest.h>
+#include <gtest/internal/gtest-internal.h>
+
+using namespace std;
+
+class Solution1 {
+public:
+  int search(const vector<int> &nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < nums[left]) {
+        right = mid;
+      } else if (nums[mid] > nums[right]) {
+        left = mid + 1;
+      } else {
+        break;
+      }
+    }
+
+    int pivot = left;
+
+    int result = binarySearch(nums, 0, pivot - 1, target);
+    if (result != -1) {
+      return result;
+    }
+
+    result = binarySearch(nums, pivot, nums.size() - 1, target);
+    if (result != -1) {
+      return result;
+    }
+
+    return -1;
+  }
+
+private:
+  int binarySearch(const vector<int> &nums, int left, int right, int target) {
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+};
+
+class Solution1Test : public ::testing::Test {
+protected:
+  Solution1 solution;
+};
+
+TEST_F(Solution1Test, TestCase1) {
+  vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+  int target = 0;
+
+  int actual = solution.search(nums, target);
+  int expected = 4;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution1Test, TestCase2) {
+  vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+  int target = 3;
+
+  int actual = solution.search(nums, target);
+  int expected = -1;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution1Test, TestCase3) {
+  vector<int> nums = {1};
+  int target = 0;
+
+  int actual = solution.search(nums, target);
+  int expected = -1;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution1Test, TestCase4) {
+  vector<int> nums = {3, 1};
+  int target = 1;
+
+  int actual = solution.search(nums, target);
+  int expected = 1;
+
+  EXPECT_EQ(actual, expected);
+}
