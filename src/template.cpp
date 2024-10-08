a354f3d5304d7ebbfa5bc34bb62ef3067db5a5b0
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-08 09:01:54 +0700

    Add template

---
+++ src/template.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution1 {
+public:
+  int add(int a, int b) { return a + b; }
+};
+
+class Solution1Test : public ::testing::Test {
+protected:
+  Solution1 solution;
+};
+
+TEST_F(Solution1Test, TestCase1) {
+  int a = 1;
+  int b = 2;
+
+  int actual = solution.add(a, b);
+  int expected = 3;
+
+  EXPECT_EQ(actual, expected);
+}
