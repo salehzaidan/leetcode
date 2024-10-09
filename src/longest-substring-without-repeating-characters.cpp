86ada44107a62b4922fb58a026f1b0e09c93db51
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-09 22:44:53 +0700

    Solve longest-substring-without-repeating-characters with hash set

--- src/longest-substring-without-repeating-characters.cpp
+++ src/longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -91,6 +92,101 @@ TEST_F(Solution1Test, TestCase6) {
 }
 
 TEST_F(Solution1Test, TestCase7) {
+  string s = "iaoioiwgrrp";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 5;
+
+  EXPECT_EQ(actual, expected);
+}
+
+class Solution2 {
+public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_set<char> chars;
+    int left = 0;
+    int right = 0;
+    int longest = 0;
+
+    while (right < s.length()) {
+      if (chars.count(s[right]) == 0) {
+        chars.insert(s[right]);
+        longest = max(longest, right - left + 1);
+      } else {
+        while (chars.count(s[right]) == 1) {
+          chars.erase(s[left]);
+          left++;
+        }
+        chars.insert(s[right]);
+      }
+
+      right++;
+    }
+
+    return longest;
+  }
+};
+
+class Solution2Test : public ::testing::Test {
+protected:
+  Solution2 solution;
+};
+
+TEST_F(Solution2Test, TestCase1) {
+  string s = "abcabcbb";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 3;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase2) {
+  string s = "bbbbb";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 1;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase3) {
+  string s = "pwwkew";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 3;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase4) {
+  string s = "";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 0;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase5) {
+  string s = "dvdf";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 3;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase6) {
+  string s = "fioiwfqp";
+
+  int actual = solution.lengthOfLongestSubstring(s);
+  int expected = 6;
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(Solution2Test, TestCase7) {
   string s = "iaoioiwgrrp";
 
   int actual = solution.lengthOfLongestSubstring(s);
