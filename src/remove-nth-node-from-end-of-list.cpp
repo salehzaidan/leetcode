f77c6dc9a1595fa2913785de7d2460f3317e8d0b
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-12 16:41:57 +0700

    Improve remove-nth-node-from-end-of-list solution
    Improve the space complexity from O(sz) to (n)
    (sz is the number of nodes in the list)

--- src/remove-nth-node-from-end-of-list.cpp
+++ src/remove-nth-node-from-end-of-list.cpp
@@ -1,5 +1,6 @@
 #include "linked_list_utils.hpp"
 #include <gtest/gtest.h>
+#include <queue>
 #include <stack>
 
 using namespace std;
@@ -74,6 +75,90 @@ TEST_F(Solution1Test, TestCase3) {
 }
 
 TEST_F(Solution1Test, TestCase4) {
+    ListNode *head = createList({1, 2});
+    int n = 2;
+
+    ListNode *actual = solution.removeNthFromEnd(head, n);
+
+    EXPECT_TRUE(compareList(actual, {2}));
+
+    deleteList(actual);
+}
+
+class Solution2 {
+  public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        queue<ListNode *> nodes;
+
+        ListNode *current = head;
+        while (current != nullptr) {
+            nodes.push(current);
+            if (nodes.size() > n + 1) {
+                nodes.pop();
+            }
+
+            current = current->next;
+        }
+
+        if (nodes.size() != n + 1) {
+            current = head->next;
+            delete head;
+            return current;
+        }
+
+        current = nodes.front();
+        ListNode *target = current->next;
+        if (target == nullptr) {
+            delete current;
+            return nullptr;
+        }
+
+        current->next = target->next;
+        delete target;
+
+        return head;
+    }
+};
+
+class Solution2Test : public ::testing::Test {
+  protected:
+    Solution2 solution;
+};
+
+TEST_F(Solution2Test, TestCase1) {
+    ListNode *head = createList({1, 2, 3, 4, 5});
+    int n = 2;
+
+    ListNode *actual = solution.removeNthFromEnd(head, n);
+
+    EXPECT_TRUE(compareList(actual, {1, 2, 3, 5}));
+
+    deleteList(actual);
+}
+
+TEST_F(Solution2Test, TestCase2) {
+    ListNode *head = createList({1});
+    int n = 1;
+
+    ListNode *actual = solution.removeNthFromEnd(head, n);
+
+    EXPECT_TRUE(compareList(actual, {}));
+
+    deleteList(actual);
+}
+
+TEST_F(Solution2Test, TestCase3) {
+    ListNode *head = createList({1, 2});
+    int n = 1;
+
+    ListNode *actual = solution.removeNthFromEnd(head, n);
+
+    EXPECT_TRUE(compareList(actual, {1}));
+
+    deleteList(actual);
+}
+
+TEST_F(Solution2Test, TestCase4) {
     ListNode *head = createList({1, 2});
     int n = 2;
 
