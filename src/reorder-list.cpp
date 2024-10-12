0ee59bb86c67059c134e97a9d91243fdf02d7e61
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-10-13 05:28:59 +0700

    Improve reorder-list solution
    Improve the space complexity to O(1)

--- src/reorder-list.cpp
+++ src/reorder-list.cpp
@@ -1,6 +1,7 @@
 #include "linked_list_utils.hpp"
 #include <gtest/gtest.h>
 #include <stack>
+#include <stdexcept>
 
 using namespace std;
 using namespace LinkedListUtils;
@@ -50,6 +51,61 @@ TEST_F(Solution1Test, TestCase1) {
 }
 
 TEST_F(Solution1Test, TestCase2) {
+    ListNode *head = createList({1, 2, 3, 4, 5});
+
+    solution.reorderList(head);
+
+    EXPECT_TRUE(compareList(head, {1, 5, 2, 4, 3}));
+
+    deleteList(head);
+}
+
+class Solution2 {
+  public:
+    void reorderList(ListNode *head) {
+        ListNode *left = head;
+        ListNode *right = head;
+        ListNode *tail = nullptr;
+
+        while (true) {
+            while (right->next != tail) {
+                right = right->next;
+            }
+
+            if (left == right) {
+                left->next = nullptr;
+                return;
+            }
+
+            ListNode *temp = left->next;
+            left->next = right;
+            left = temp;
+
+            right->next = left;
+            tail = right;
+            right = left;
+        }
+
+        throw runtime_error("Unreachable");
+    }
+};
+
+class Solution2Test : public ::testing::Test {
+  protected:
+    Solution2 solution;
+};
+
+TEST_F(Solution2Test, TestCase1) {
+    ListNode *head = createList({1, 2, 3, 4});
+
+    solution.reorderList(head);
+
+    EXPECT_TRUE(compareList(head, {1, 4, 2, 3}));
+
+    deleteList(head);
+}
+
+TEST_F(Solution2Test, TestCase2) {
     ListNode *head = createList({1, 2, 3, 4, 5});
 
     solution.reorderList(head);
